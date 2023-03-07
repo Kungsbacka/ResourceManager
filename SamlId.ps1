cbf5bcd4fae6fb306eb362de18373bc4bce40367
Jonas Sjömark <jonas.sjomark@kungsbacka.se>
2023-03-07 16:37:53 +0100

    Get SAML-ID from pool

--- SamlId.ps1
+++ SamlId.ps1
@@ -1,68 +1,8 @@
 ﻿# Make all error terminating errors
 $Global:ErrorActionPreference = 'Stop'
 
-function New-RmSamlId
+function Get-SamlIdFromPool
 {
-    param
-    (
-        [Parameter(Mandatory=$true,ValueFromPipeline=$true)]
-        [string]
-        $Domain
-    )
-    $rng = [System.Security.Cryptography.RNGCryptoServiceProvider]::Create()
-    [byte[]]$bytes = [byte[]]::new(4)
-    $sb = [System.Text.StringBuilder]::new()
-
-    # Borrowed from the CryptoRandom class found here:
-    # https://docs.microsoft.com/en-us/archive/msdn-magazine/2007/september/net-matters-tales-from-the-cryptorandom
-    function Rnd([int32]$minValue, [int32]$maxValue)
-    {
-        if ($minValue -gt $maxValue)
-        {
-            throw 'Min cannot be greater than max'
-        }
-        if ($minValue -eq $maxValue)
-        {
-            $minValue
-            return
-        }
-        [int64]$diff = $maxValue - $minValue
-        while ($true)
-        {
-            $rng.GetBytes($bytes)
-            [uint32]$rand = [System.BitConverter]::ToUInt32($bytes, 0)
-            [int64]$max = 1 + [long][int32]::MaxValue
-            [int64]$remainder = $max % $diff
-            if ($rand -lt $max - $remainder)
-            {
-                [int32]($minValue + ($rand % $diff))
-                return
-            }
-        }
-    }
-
-    $set = 'abcdefghijkmnpqrstuvxyz23456789'
-    $len = $set.Length
-    for ($i = 0; $i -lt 16; $i++) {
-        $n = Rnd -minValue 0 -maxValue $len
-        $null = $sb.Append($set[$n])
-    }
-    if ($Domain -notlike '@*')
-    {
-        $null = $sb.Append('@')
-    }
-    $null = $sb.Append($Domain)
-    $sb.ToString()
-}
-
-function Test-RmSamlId
-{
-    param
-    (
-        [Parameter(Mandatory=$true,ValueFromPipeline=$true)]
-        [string]
-        $SamlId
-    )
     try
     {
         $conn = New-Object -TypeName 'System.Data.SqlClient.SqlConnection' -ArgumentList @($Script:Config.MetaDirectory.ConnectionString)
@@ -70,15 +10,11 @@ function Test-RmSamlId
         $cmd = New-Object -TypeName 'System.Data.SqlClient.SqlCommand'
         $cmd.Connection = $conn
         $cmd.CommandType = 'StoredProcedure'
-        $cmd.CommandText = 'dbo.spIsSamlIdReserved'
-        $null = $cmd.Parameters.AddWithValue('@samlId', $SamlId)
-        $param = $cmd.Parameters.Add('@rc', 'int')
-        $param.Direction = 'ReturnValue'
-        $null = $cmd.ExecuteNonQuery()
-        if ($cmd.Parameters["@rc"].Value -eq 1) # 1 = SAML-ID i reserved and cannot be reused
+        $cmd.CommandText = 'dbo.spGetSamlIdFromPool'
+        $rdr = $cmd.ExecuteReader()
+        if ($rdr.Read())
         {
-            $false
-            return
+            $rdr.GetString(0)
         }
     }
     finally
@@ -94,14 +30,6 @@ function Test-RmSamlId
             $conn = $null
         }
     }
-
-    if ((Get-ADUser -Filter "msDS-cloudExtensionAttribute14 -eq '$SamlId'"))
-    {
-        $false
-        return
-    }
-
-    $true
 }
 
 function Set-RmSamlId
@@ -120,11 +48,7 @@ function Set-RmSamlId
     {
         throw "msDS-cloudExtensionAttribute14 is not empty. Use Force to overwrite."
     }
-    do
-    {
-        $samlId = New-RmSamlId -Domain $Script:Config.SamlId.Domain
-    }
-    while (-not (Test-RmSamlId -SamlId $samlId))
+    $samlId = Get-SamlIdFromPool
     if ($hasValue)
     {
         if ($Force)
